776142c4bf848411816b88afca49db29890b3e01
Yang Shun <tay.yang.shun@gmail.com>
2013-12-09 20:51:36 +0800

    Shoot bubble interaction

--- Puzzle Bobble/ViewController.m
+++ Puzzle Bobble/ViewController.m
@@ -10,9 +10,16 @@
 #import "Bubble.h"
 
 #define NUMBER_OF_BUBBLES_IN_ROW 8
+#define SCREEN_WIDTH 768.f
+#define SCREEN_HEIGHT 1024.f
+#define BUBBLE_DIAMETER SCREEN_WIDTH/NUMBER_OF_BUBBLES_IN_ROW
 
 @interface ViewController () {
     NSMutableArray *bubblesArray;
+    NSTimer *timer;
+    Bubble *activeBubble;
+    CGPoint activeBubbleVelocity;
+    UITapGestureRecognizer *tapRecognizer;
 }
 
 @end
@@ -25,29 +32,72 @@
 	// Do any additional setup after loading the view, typically from a nib.
     bubblesArray = [NSMutableArray array];
     [self initializeBubbles];
+    tapRecognizer = [[UITapGestureRecognizer alloc]
+                                             initWithTarget:self action:@selector(shootBubble:)];
+    
+    // Specify that the gesture must be a single tap
+    tapRecognizer.numberOfTapsRequired = 1;
+    
+    // Add the tap gesture recognizer to the view
+    [self.view addGestureRecognizer:tapRecognizer];
 }
 
 - (void)initializeBubbles {
     
-    CGFloat bubbleDiameter = self.view.frame.size.width/NUMBER_OF_BUBBLES_IN_ROW;
-    CGFloat rowOffset = bubbleDiameter * sin(M_PI/3);
+    CGFloat rowOffset = BUBBLE_DIAMETER * sin(M_PI/3);
 
-    for (int j = 0; j < 7; j++) {
+    for (int j = 0; j < 4; j++) {
         for (int i = 0; i < NUMBER_OF_BUBBLES_IN_ROW; i++) {
-            Bubble *bubble = [[Bubble alloc] initWithFrame:CGRectMake(0, 0, bubbleDiameter, bubbleDiameter)];
+            Bubble *bubble = [[Bubble alloc] initWithFrame:CGRectMake(0, 0, BUBBLE_DIAMETER, BUBBLE_DIAMETER)];
             bubble.backgroundColor = [UIColor redColor];
-            bubble.layer.cornerRadius = bubbleDiameter/2;
+            bubble.layer.cornerRadius = BUBBLE_DIAMETER/2;
             [self.view addSubview:bubble];
             [bubblesArray addObject:bubble];
             if (j % 2 == 0) {
-                bubble.center = CGPointMake(i * bubbleDiameter + bubbleDiameter/2,
-                                            j * rowOffset + bubbleDiameter/2);
+                bubble.center = CGPointMake(i * BUBBLE_DIAMETER + BUBBLE_DIAMETER/2,
+                                            j * rowOffset + BUBBLE_DIAMETER/2);
             } else {
                 // odd rows have 1 less bubble
-                bubble.center = CGPointMake((i+1) * bubbleDiameter,
-                                            j * rowOffset + bubbleDiameter/2);
+                bubble.center = CGPointMake((i+1) * BUBBLE_DIAMETER,
+                                            j * rowOffset + BUBBLE_DIAMETER/2);
                 if (i == NUMBER_OF_BUBBLES_IN_ROW - 2) { break; }
             }
+        }
+    }
+}
+
+- (void)shootBubble:(UITapGestureRecognizer *)recognizer {
+    
+    CGPoint dest = [recognizer locationInView:self.view];
+    CGPoint start = CGPointMake(SCREEN_WIDTH/2, SCREEN_HEIGHT - BUBBLE_DIAMETER/2);
+    
+    activeBubbleVelocity = CGPointMake(-1*(dest.x - start.x)/(dest.y - start.y), -1);
+    
+    activeBubble = [[Bubble alloc] initWithFrame:CGRectMake(0, 0, BUBBLE_DIAMETER, BUBBLE_DIAMETER)];
+    activeBubble.center = start;
+    activeBubble.backgroundColor = [UIColor blueColor];
+    activeBubble.layer.cornerRadius = BUBBLE_DIAMETER/2;
+    [self.view addSubview:activeBubble];
+    [timer invalidate];
+    timer = [NSTimer scheduledTimerWithTimeInterval:0.001f
+                                    target:self
+                                  selector:@selector(moveActiveBubble)
+                                  userInfo:nil
+                                   repeats:YES];
+    tapRecognizer.enabled = NO;
+}
+
+- (void)moveActiveBubble {
+
+    activeBubble.center = CGPointMake(activeBubbleVelocity.x + activeBubble.center.x,
+                                      activeBubbleVelocity.y + activeBubble.center.y);
+    for (Bubble *b in bubblesArray) {
+        CGFloat dist = sqrtf(powf(activeBubble.center.x - b.center.x, 2) + powf(activeBubble.center.y - b.center.y, 2));
+        if (dist < BUBBLE_DIAMETER) {
+            [timer invalidate];
+            [bubblesArray addObject:activeBubble];
+            tapRecognizer.enabled = YES;
+            break;
         }
     }
 }
