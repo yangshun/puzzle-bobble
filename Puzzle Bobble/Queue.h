3680afb0605c6a66edd4fe64574f7d01fc9be77a
Yang Shun <tay.yang.shun@gmail.com>
2013-12-10 17:17:08 +0800

    Queue implementation added

--- Puzzle Bobble/Queue.h
+++ Puzzle Bobble/Queue.h
@@ -10,4 +10,19 @@
 
 @interface Queue : NSObject
 
+// adds an object to the back of the queue
+- (void)enqueue:(id)object;
+
+// removes an object from the front of the queue and returns it
+- (id)dequeue;
+
+// returns the objects at the front of the queue
+- (id)examine;
+
+// returns YES if the queue is empty
+- (BOOL)isEmpty;
+
+// returns the number of objects in the queue
+- (int)length;
+
 @end
--- Puzzle Bobble/Queue.m
+++ Puzzle Bobble/Queue.m
@@ -8,6 +8,56 @@
 
 #import "Queue.h"
 
-@implementation Queue
+@implementation Queue {
+    NSMutableArray *items;
+}
+
+- (id)init {
+    
+    self = [super init];
+    if (self) {
+        items = [NSMutableArray array];
+    }
+    return self;
+}
+
+
+// adds an object to the back of the queue
+- (void)enqueue:(id)object {
+    [items addObject:object];
+}
+
+// removes an object from the front of the queue and returns it
+// returns nil if queue is empty
+- (id)dequeue {
+    if (![self isEmpty]) {
+        id object = [items objectAtIndex:0];
+        [items removeObjectAtIndex:0];
+        return object;
+    } else {
+        return nil;
+    }
+}
+
+// returns the objects at the front of the queue
+// returns nil if queue is empty
+- (id)examine {
+    if (![self isEmpty]) {
+        return [items objectAtIndex:0];
+    } else {
+        return nil;
+    }
+}
+
+// returns YES if the queue is empty
+- (BOOL)isEmpty {
+    return [self length] == 0;
+}
+
+// returns the number of objects in the queue
+- (int)length {
+    return items.count;
+}
+
 
 @end
