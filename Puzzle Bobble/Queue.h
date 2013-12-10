982c036b2199def887b8d3337e6581120bfb1096
Yang Shun <tay.yang.shun@gmail.com>
2013-12-10 17:16:24 +0800

    Queue data structure added

---
+++ Puzzle Bobble/Queue.h
@@ -0,0 +1,13 @@
+//
+//  Queue.h
+//  Puzzle Bobble
+//
+//  Created by YangShun on 10/12/13.
+//  Copyright (c) 2013 YangShun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Queue : NSObject
+
+@end
---
+++ Puzzle Bobble/Queue.m
@@ -0,0 +1,13 @@
+//
+//  Queue.m
+//  Puzzle Bobble
+//
+//  Created by YangShun on 10/12/13.
+//  Copyright (c) 2013 YangShun. All rights reserved.
+//
+
+#import "Queue.h"
+
+@implementation Queue
+
+@end
